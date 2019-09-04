6013a9426e2971392a74bf44afa0c03575a2fc65
GreatWhiteJ <48426180+GreatWhiteJ@users.noreply.github.com>
2019-09-04 17:12:02 -0600

    Firebase
    Loads directly to firebase via travis

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyBSZ7NUCTYQA7wQP510iXuX0pZz-E1zJkY",
+  authDomain: "authentication-b6f37.firebaseapp.com",
+  databaseURL: "https://authentication-b6f37.firebaseio.com",
+  projectId: "authentication-b6f37",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "780615393858",
+  appId: "1:780615393858:web:67c2a797d096e5516483db"
 };
 
 firebase.initializeApp(config);
